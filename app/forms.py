2cb5414581cdc5fbff26b961350405a58894d696
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-16 10:40:48 +0300

    Instantiate the variables that will hold our wtform inputs

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,14 @@
 import email
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 
 
 class RegistrationForm(FlaskForm) :
-  username
-  email
-  password
-  confirm_password
-  submit
+  username = StringField(label='Username', validators=[DataRequired(), Length(min=3,max=20)])
+  email = StringField(label='Email', validators=[DataRequired(), Email()])
+  password = PasswordField(label='Password', validators=[DataRequired(), Length(min=5,max=16)])
+  confirm_password = PasswordField(label='Confirm password', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField(label='REGISTER')
