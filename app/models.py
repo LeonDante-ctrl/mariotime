ca5e34507185feebc86e28c9ed9645115512cb07
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-17 12:10:13 +0300

    Adjust the password string to 60 characters to align with the hashing

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(db.Model) :
   username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(30), default='default.jpg', nullable=False)
-  password = db.Column(db.String(20), nullable=False)
+  password = db.Column(db.String(60), nullable=False)
   # date_created = db.Column(db.Datetime, default=datetime.utcnow)
 
   def __repr__(self) :
