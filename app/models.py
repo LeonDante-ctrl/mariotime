077010302fc707525b7e2d8334a010e6791f7296
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-17 08:41:56 +0300

    Create an SQLite database file via the command line

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from datetime import datetime
 # import email
 # from email.policy import default
 
 # from click import password_option
 from app import db
+from datetime import datetime
 
 
 
@@ -14,7 +14,7 @@ class User(db.Model) :
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(30), default='default.jpg', nullable=False)
   password = db.Column(db.String(20), nullable=False)
-  date_created = db.Column(db.Datetime, default=datetime.utcnow)
+  # date_created = db.Column(db.Datetime, default=datetime.utcnow)
 
   def __repr__(self) :
       return f'{self.username} : {self.email} : {self.date_created}'
