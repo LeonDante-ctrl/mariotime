dba4d98b32ee6d0e89513b8bbf06e77525aa09c3
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-17 08:07:21 +0300

    Add SQLite configuration and initialize the SQLAlchemy db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 # App initialization
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
+
+
+# app configurations
 app.config['SECRET_KEY']='mariotimeshaviya'
+
+# configuring the database for SQLLite
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database/mario.db'
+
+
+
 
 
 from app import views
