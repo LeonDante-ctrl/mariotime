177544f7710c6e0b3bc6a40b95a9df22bc1ed3dd
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-17 11:57:11 +0300

    Initialize the Brypt for password encryption

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 
 
@@ -7,6 +9,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 
 
